7e7178611dc4e8d17ba98257e1fba4567537fe2b
s00464087 <s00464087@HGHY2S004640871.china.huawei.com>
2019-08-19 16:06:40 +0800

    IC-2019.1.3 <s00464087@HGHY2S004640871 Create IntelliLang.xml, print.xml, project.default.xml, diff.xml, Enum.java, Default copy.xml, Interface.java, Default.xml, applicationLibraries.xml, find.xml, File Header.java, keymap.xml, code.style.schemes.xml, log.xml, mavenVersion.xml, Default.xml, customPostfixTemplates.xml, ECS.xml, editor.codeinsight.xml, editor.xml, Bash.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, _@user_Default.icls, File Copyright.java, getterTemplates.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, projectView.xml, javaeeExternalResources.xml, Java.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, AnnotationType.java, stringManipulation.xml, output.xml, laf.xml, Eclipse Custom.xml, Class.java, jdk.table.xml, path.macros.xml, mavenRunHelper.xml, web-browsers.xml, proxy.settings.xml, dbnavigator.xml, toStringTemplates.xml, usageView.xml, certificates.xml, setterTemplates.xml

---
+++ fileTemplates/includes/File Copyright.java
@@ -0,0 +1,5 @@
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
